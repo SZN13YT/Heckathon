9c6a70db16b3fe0d842531570cf2bbfa4bdface6
SZN13YT <szabo.nandor.istvan@gmail.com>
2025-05-23 23:51:19 +0200

    Update main.py

--- fibonacci/main.py
+++ fibonacci/main.py
@@ -1,4 +1,13 @@
 with open('./input.txt', 'r') as f:
   input = f.read()
 
+input = input.strip().split()
+def fibonacci(n):
+  if n == 0 or n == 1: return n
+  elif 0 <= n <= int(i): return fibonacci(n-1) + fibonacci(n-2)
+  print(n)
+for i in input:
+  if i.isnumeric():
+    print(fibonacci(10))
+  print()
 print(input)
