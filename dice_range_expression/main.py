50a6e955196fcbcffd5373cd0b3207f9c9fb1e16
SZN13YT <szabo.nandor.istvan@gmail.com>
2025-05-25 23:15:54 +0200

    Update main.py

--- dice_range_expression/main.py
+++ dice_range_expression/main.py
@@ -1,30 +1,33 @@
 with open('c:/Users/nando/Desktop/Heckathon/dice_range_expression/input.txt', 'r') as f:
   input = [[int(j) for j in i.strip().split()] for i in f.readlines()]
-
-print(input)
-
-def what_dice(n):
+def what_dice(n1, n0, nn):
   r = {20: 0, 10: 0, 8: 0, 6: 0, 4: 0, 3: 0, 2: 0}
+  n = n1 - n0 + nn
   if n in r:
     r[n] += 1
-    return r
-  
+    return [r, n0 - nn]
+  d = 0
   for _ in r:
     if n - _ == 0:
+      d += _
       r[_] += 1
-      return r
+      return [r, n0 - nn]
     elif n - _ > 0:
+      d += _
       r[_] += 1
       n -= _
-  return r
+  return [r, n0 - nn]
 
 for i in input:
+  pr = False
   if i[0] > 1:
-    out, ch = what_dice(i[1] - i[0] + 1)
-    print(out, ch)
+    out = what_dice(i[1], i[0], 1)
   else: 
-    out = what_dice(i[1] - i[0] + 2)
-  for i in out:
-    if out.get(i) != 0:
-      print(f"{out.get(i)}d{i}", end="")
-  print()
+    out = what_dice(i[1], i[0], 2)
+  for i in [[j for j in out[0].keys()][_] for _ in range(-1, -len(out[0])-1, -1)]:
+    if out[0].get(i) != 0:
+      if not pr:
+        pr = True
+        print(f"{out[0].get(i)}d{i}", end="")
+      else: print(f"+{out[0].get(i)}d{i}", end="")
+  if out[1] != 0: print(f"+{out[1]}" if out[1] > 0 else f"{out[1]}")
