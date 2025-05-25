c76837d5053fd263054a6b3a1c0cf04fe2aaad30
SZN13YT <szabo.nandor.istvan@gmail.com>
2025-05-25 15:59:09 +0200

    fibonacci

--- fibonacci/main.py
+++ fibonacci/main.py
@@ -3,11 +3,19 @@ with open('./input.txt', 'r') as f:
 
 input = input.strip().split()
 def fibonacci(n):
-  if n == 0 or n == 1: return n
-  elif 0 <= n <= int(i): return fibonacci(n-1) + fibonacci(n-2)
-  print(n)
+  b = 0
+  next = 1
+  back = ""
+  while 0 <= b <= n:
+    if b % 3 == 0: back += f"{b} "
+    a, b = b, next
+    next = a + b
+  return back.strip().split()
 for i in input:
-  if i.isnumeric():
-    print(fibonacci(10))
-  print()
-print(input)
+  try:
+    if int(i) >= 0:
+      fibo = fibonacci(int(i))
+      for _ in range(len(fibo)):
+        if _ < len(fibo) -1: print(fibo[_], end=", ")
+        else: print(fibo[_])
+  except: continue
