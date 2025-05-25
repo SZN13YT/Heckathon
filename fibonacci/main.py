d0152ef1bbbc0fce8feacda13fa4f3e74855f482
SZN13YT <szabo.nandor.istvan@gmail.com>
2025-05-25 16:05:44 +0200

    Update main.py

--- fibonacci/main.py
+++ fibonacci/main.py
@@ -13,11 +13,11 @@ def fibonacci(n):
   return back.strip().split()
 for i in input:
   try:
-    if int(i) >= 0:
-      fibo = fibonacci(int(i))
-      if len(fibo) > 1:
+    if int(round(float(i), 0)) >= 0:
+      fibo = fibonacci(int(round(float(i), 0)))
+      if len(fibo) > 0:
         for _ in range(len(fibo)):
           if _ < len(fibo) -1: print(fibo[_], end=", ")
           else: print(fibo[_])
       else: print("N/A")
-  except: print("N/A")
+  except: print("N/A", i)
