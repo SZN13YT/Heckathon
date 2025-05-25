007989f53fab1a8eddda614e6dd3332643eae8c0
SZN13YT <szabo.nandor.istvan@gmail.com>
2025-05-25 22:49:19 +0200

    Update main.py

--- dice_range_expression/main.py
+++ dice_range_expression/main.py
@@ -1,5 +1,30 @@
-with open('./input.txt', 'r') as f:
-  input = f.read()
+with open('c:/Users/nando/Desktop/Heckathon/dice_range_expression/input.txt', 'r') as f:
+  input = [[int(j) for j in i.strip().split()] for i in f.readlines()]
 
-dices = [20, 10, 8, 6, 4, 3, 2]
-print(dices)
+print(input)
+
+def what_dice(n):
+  r = {20: 0, 10: 0, 8: 0, 6: 0, 4: 0, 3: 0, 2: 0}
+  if n in r:
+    r[n] += 1
+    return r
+  
+  for _ in r:
+    if n - _ == 0:
+      r[_] += 1
+      return r
+    elif n - _ > 0:
+      r[_] += 1
+      n -= _
+  return r
+
+for i in input:
+  if i[0] > 1:
+    out, ch = what_dice(i[1] - i[0] + 1)
+    print(out, ch)
+  else: 
+    out = what_dice(i[1] - i[0] + 2)
+  for i in out:
+    if out.get(i) != 0:
+      print(f"{out.get(i)}d{i}", end="")
+  print()
